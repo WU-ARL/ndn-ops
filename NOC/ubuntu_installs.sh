5b5387ace12505ea7b8d8e9662fea140f2db9c08
John DeHart <ndnops@wundngw.arl.wustl.edu>
2013-10-30 11:51:41 -0500

    first commit

---
+++ NOC/ubuntu_installs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+sudo apt-get update
+sudo apt-get -y install build-essential ant git openjdk-7-jdk openjdk-7-jre openssl python-openssl libssl-dev expat libexpat-dev libpcap-dev
+sudo apt-get -y install gawk texinfo
---
+++ named-data/v0.1/git_and_install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# What we need for a Gateway Node:
+#    ndnx
+#    OSPFN3.0
+#      configuration files for OSPFN3.0
+#
+
+export  JAVA_HOME="/usr"
+
+#git clone http://github.com/named-data/ndnx
+#pushd ndnx
+#git checkout tags/v0.1
+#./configure
+#make
+#make test
+##make install
+#popd
+#
+#git clone http://github.com/named-data/OSPFN3.0
+sudo addusr quagga
+pushd /usr/local/etc/
+sudo mkdir quagga
+sudo chown quagga:quagga quagga
+sudo chmod g+w quagga
+popd
+pushd /var/run
+sudo mkdir quagga-state
+sudo chown quagga:quagga quagga-state
+sudo chmod g+w quagga-state
+popd
+
+pushd OSPFN3.0
+./configure --enable-opaque-lsa --disable-ipv6 --disable-ripd --disable-ripngd --disable-ospf6d --disable-bgpd --disable-bgp-announce --sysconfdir=/usr/local/etc/quagga --localstatedir=/var/run/quagga-state
+make
+#make install
+
