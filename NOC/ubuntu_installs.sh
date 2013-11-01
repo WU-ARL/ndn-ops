e71abff2c47848d2e2860bddcf07a37a0e16f2d6
John DeHart <ndnops@wundngw.arl.wustl.edu>
2013-11-01 10:42:31 -0500

    changing to open jdk version 6 (from 7) same for jre

--- NOC/ubuntu_installs.sh
+++ NOC/ubuntu_installs.sh
@@ -2,5 +2,5 @@
 
 
 sudo apt-get update
-sudo apt-get -y install build-essential ant git openjdk-7-jdk openjdk-7-jre openssl python-openssl libssl-dev expat libexpat-dev libpcap-dev
+sudo apt-get -y install build-essential ant git openjdk-6-jdk openjdk-6-jre openssl python-openssl libssl-dev expat libexpat-dev libpcap-dev
 sudo apt-get -y install gawk texinfo
