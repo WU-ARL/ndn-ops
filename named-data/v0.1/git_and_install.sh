d57ee36aaa3b1fa4002d9852817807b490a0f92d
John DeHart <ndnops@wundngw.arl.wustl.edu>
2013-11-02 15:39:08 -0500

    uncomment ndnx stuff

--- named-data/v0.1/git_and_install.sh
+++ named-data/v0.1/git_and_install.sh
@@ -8,16 +8,16 @@
 
 export  JAVA_HOME="/usr"
 
-#git clone http://github.com/named-data/ndnx
-#pushd ndnx
-#git checkout tags/v0.1
-#./configure
-#make
-#make test
-##make install
-#popd
-#
-#git clone http://github.com/named-data/OSPFN3.0
+git clone http://github.com/named-data/ndnx
+pushd ndnx
+git checkout tags/v0.1
+./configure
+make
+make test
+#make install
+popd
+
+git clone http://github.com/named-data/OSPFN3.0
 sudo addusr quagga
 pushd /usr/local/etc/
 sudo mkdir quagga
