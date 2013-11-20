d1fbfd5ff6fc5e41416b38abd363d0465f7bf45c
John DeHart <ndnops@wundngw.arl.wustl.edu>
2013-11-20 12:49:38 -0600

    add asciidoc to installs

--- NOC/ubuntu_installs.sh
+++ NOC/ubuntu_installs.sh
@@ -3,4 +3,4 @@
 
 sudo apt-get update
 sudo apt-get -y install build-essential ant git openjdk-6-jdk openjdk-6-jre openssl python-openssl libssl-dev expat libexpat-dev libpcap-dev
-sudo apt-get -y install gawk texinfo
+sudo apt-get -y install gawk texinfo asciidoc
