6001eaaa5e8e6a1ac8247da7c4dbab1c355c40f9
jdehart1 <jdd@wustl.edu>
2013-11-27 12:47:36 -0600

    add ndn security packages and installs

--- NOC/ubuntu_installs.sh
+++ NOC/ubuntu_installs.sh
@@ -4,3 +4,6 @@
 sudo apt-get update
 sudo apt-get -y install build-essential ant git openjdk-6-jdk openjdk-6-jre openssl python-openssl libssl-dev expat libexpat-dev libpcap-dev
 sudo apt-get -y install gawk texinfo asciidoc finger
+
+sudo apt-get -y install python-software-properties
+
---
+++ ndn-security/git_and_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# What we need for a NDN Security:
+#    Assume ndnx is already installed
+#
+#    python-ndn-exp
+#    ndn.cxx-dev 
+#    ndn.cxx-security-tools 
+#    ndn-testbed-operator-tools
+
+#git clone https://github.com/named-data/ndn.cxx.git
+#
+#git clone https://github.com/named-data/ndnx.cxx-security-tools.git
+
+# python-ndn-exp
+sudo add-apt-repository -y ppa:named-data/ppa
+sudo apt-get update
+sudo apt-get install ndn.cxx-dev ndn.cxx-security-tools python-ndn-exp
+
+
+
+# install operator ndnsec tools:
+git clone http://github.com/cawka/ndn-testbed-operator-tools
+pushd ndn-testbed-operator-tools
+sudo cp ndnop-process-requests /usr/local/bin
+
