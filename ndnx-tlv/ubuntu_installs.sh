9c415c91aae37f39ce7f3a3b3fe049bcb281364e
jdehart1 <jdd@wustl.edu>
2014-02-13 09:47:06 -0600

    add pkg-config

--- ndnx-tlv/ubuntu_installs.sh
+++ ndnx-tlv/ubuntu_installs.sh
@@ -3,5 +3,6 @@
 
 sudo apt-get update
 sudo apt-get -y install build-essential
+sudo apt-get -y install pkg-config
 sudo apt-get -y install libboost-all-dev libssl-dev libsqlite3-dev libcrypto++-dev
 
