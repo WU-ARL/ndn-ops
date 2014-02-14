64d673bd2202d775325ceea85da3d0899263c7fc
jdehart1 <jdd@wustl.edu>
2014-02-13 18:51:42 -0800

    update the boost all dev library

--- ndnx-tlv/ubuntu_installs.sh
+++ ndnx-tlv/ubuntu_installs.sh
@@ -4,5 +4,5 @@
 sudo apt-get update
 sudo apt-get -y install build-essential
 sudo apt-get -y install pkg-config
-sudo apt-get -y install libboost-all-dev libssl-dev libsqlite3-dev libcrypto++-dev
+sudo apt-get -y install libboost1.48-all-dev libssl-dev libsqlite3-dev libcrypto++-dev
 
