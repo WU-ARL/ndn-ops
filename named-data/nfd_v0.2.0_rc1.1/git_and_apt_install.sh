3d4d15706928239bc10e6730b6fc5b90abf326b4
jdehart1 <jdd@wustl.edu>
2014-07-24 10:50:06 -0500

    more key/cert and NLSR info

--- named-data/nfd_v0.2.0_rc1.1/git_and_apt_install.sh
+++ named-data/nfd_v0.2.0_rc1.1/git_and_apt_install.sh
@@ -14,3 +14,12 @@ sudo apt-get install ndn-cxx nfd nlsr
 sudo apt-get install ndn-cxx-dev
 sudo apt-get install ndndump
 sudo apt-get install ndn-tlv-ping
+
+# set stuff up for NLSR
+# We need 
+#   1. A config file specific to this node
+#   2. keys generated for this node, router and operator
+# The above two steps will be done manually by network installer
+# when a new node is brought up.
+
+sudo mkdir -p /etc/ndn/nlsr/keys
