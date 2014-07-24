aa206631ee49f3e4af588a4242d8d18165b6a9ab
jdehart1 <jdd@wustl.edu>
2014-07-24 11:03:40 -0700

    more nlsr config and adding upstart for nlsr

--- named-data/nfd_v0.2.0_rc1.1/git_and_apt_install.sh
+++ named-data/nfd_v0.2.0_rc1.1/git_and_apt_install.sh
@@ -24,3 +24,6 @@ sudo apt-get install ndn-tlv-ping
 
 sudo mkdir -p /etc/ndn/nlsr/keys
 sudo mkdir -p /var/lib/ndn/nlsr
+sudo mkdir -p /var/log/ndn/nlsr
+sudo chown ndn.ndn /var/lib/ndn/nlsr /var/log/ndn/nlsr 
+
