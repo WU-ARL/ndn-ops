57c85a55cab21032cc0280ae2b8f14afb5b6541d
jdehart1 <jdd@wustl.edu>
2014-07-25 12:14:31 -0400

    use user nlsr for keys

--- named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/bupt.sh
+++ named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/bupt.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-ndnsec-key-gen -n /ndn/cn/edu/bupt > unsigned_site.cert
+# point home to /var/lib/ndn/nlsr so keys will be stored there.
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/bupt > ~nlsr/unsigned_site.cert'
 
 # this next step must take place on UCLA where the root cert is.
 #ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "BUPT" -s /ndn -p /ndn/cn/edu/bupt -r unsigned_site.cert > site.cert
 
-ndnsec-key-gen -n /ndn/cn/edu/bupt/%C1.Operator/ndnops > unsigned_operator.cert
-ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "BUPT Operator" -s /ndn/cn/edu/bupt -p /ndn/cn/edu/bupt/%C1.Operator/ndnops -r unsigned_operator.cert > operator.cert
-ndnsec-key-gen -n /ndn/cn/edu/bupt/%C1.Router/bupthub > unsigned_router.cert
-ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "BUPT Router bupthub" -s /ndn/cn/edu/bupt/%C1.Operator/ndnops -p /ndn/cn/edu/bupt/%C1.Router/bupthub -r unsigned_router.cert > router.cert
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/bupt/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "BUPT Operator" -s /ndn/cn/edu/bupt -p /ndn/cn/edu/bupt/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > ~nlsr/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/bupt/%C1.Router/bupthub > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "BUPT Router bupthub" -s /ndn/cn/edu/bupt/%C1.Operator/ndnops -p /ndn/cn/edu/bupt/%C1.Router/bupthub -r ~nlsr/unsigned_router.cert > ~nlsr/router.cert'
