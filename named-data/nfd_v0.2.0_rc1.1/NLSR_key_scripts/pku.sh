03b5ae50a59cd5ab56e2ef12c43ccde366ba5a5a
jdehart1 <jdd@wustl.edu>
2014-07-25 17:33:12 -0400

    add PKU config files

--- named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/pku.sh
+++ named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/pku.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-ndnsec-key-gen -n /ndn/cn/edu/pku > unsigned_site.cert
-ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU" -s /ndn -p /ndn/cn/edu/pku -r unsigned_site.cert > site.cert
-ndnsec-key-gen -n /ndn/cn/edu/pku/%C1.Operator/ndnops > unsigned_operator.cert
-ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU Operator" -s /ndn/cn/edu/pku -p /ndn/cn/edu/pku/%C1.Operator/ndnops -r unsigned_operator.cert > operator.cert
-ndnsec-key-gen -n /ndn/cn/edu/pku/%C1.Router/pkuhub > unsigned_router.cert
-ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU Router pkuhub" -s /ndn/cn/edu/pku/%C1.Operator/ndnops -p /ndn/cn/edu/pku/%C1.Router/pkuhub -r unsigned_router.cert > router.cert
+# point home to /var/lib/ndn/nlsr so keys will be stored there.
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/pku > ~nlsr/unsigned_site.cert'
+
+# this next step must take place on UCLA where the root cert is.
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU" -s /ndn -p /ndn/cn/edu/pku -r pku_unsigned_site.cert > /home/nlsr/site.cert'
+
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/pku/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU Operator" -s /ndn/cn/edu/pku -p /ndn/cn/edu/pku/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > ~nlsr/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/pku/%C1.Router/pkuhub > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "PKU Router pkuhub" -s /ndn/cn/edu/pku/%C1.Operator/ndnops -p /ndn/cn/edu/pku/%C1.Router/pkuhub -r ~nlsr/unsigned_router.cert > ~nlsr/router.cert'
