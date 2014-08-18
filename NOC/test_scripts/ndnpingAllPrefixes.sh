566e34c4fd62b6fc0a7b453b91c3445a84d54d59
jdehart1 <jdd@wustl.edu>
2014-08-18 14:43:22 -0700

    redirect output

--- NOC/test_scripts/ndnpingAllPrefixes.sh
+++ NOC/test_scripts/ndnpingAllPrefixes.sh
@@ -2,9 +2,11 @@
 
 PREFIXES=`cat prefixes.txt`
 
+rm -f ndnping.log
 for p in $PREFIXES
 do
 
-  echo "Testing prefix: $p"
-  ndnping -c 5 $p
+  #echo "Testing prefix: $p"
+  ndnping -c 5 $p >> ndnping.log
 done
+mv ndnping.log ndnping.log.export
