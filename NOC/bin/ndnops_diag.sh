8d9c6f9b941f5dcd5317d4b6ba1fd7c6129b3576
jdehart1 <jdd@wustl.edu>
2014-10-06 16:06:39 -0500

    add diag script

---
+++ NOC/bin/ndnops_diag.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+  NAME=$1
+  OUTPUT_FILE="/home/ndnops/ndn-ops/NOC/bin/ndnops_diag.log"
+else
+  if [ $# -eq 2 ]
+  then
+    NAME=$1
+    OUTPUT_FILE=$2
+  
+  else
+    NAME="UNKNOWN"
+    OUTPUT_FILE="/home/ndnops/ndn-ops/NOC/bin/ndnops_diag.log"
+  fi
+fi
+
+DATE=`date -u` 
+
+
+NFD_VSIZE=`ps alx | sed -n '/\/usr\/bin\/nfd /s/ \+/ /gp' | cut -d ' ' -f 7`
+NFD_PID=`ps alx | sed -n '/\/usr\/bin\/nfd /s/ \+/ /gp' | cut -d ' ' -f 3`
+NFD_LOGFILE_SIZE=`du -sh /var/log/ndn/nfd.log | cut -d '	' -f 1`
+read ROOT_DISK_SIZE ROOT_DISK_USED ROOT_DISK_AVAIL <<< $(df -h / | grep -v Filesystem | sed -n '/ /s/ \+/ /gp' | cut -d ' ' -f 2,3,4)
+
+#echo "NFD_PID=$NFD_PID" >> $OUTPUT_FILE
+#echo "NFD_VSIZE=$NFD_VSIZE" >> $OUTPUT_FILE
+#echo "NFD_LOGFILE_SIZE=$NFD_LOGFILE_SIZE" >> $OUTPUT_FILE
+#echo "ROOT_DISK_SIZE=$ROOT_DISK_SIZE" >> $OUTPUT_FILE
+#echo "ROOT_DISK_USED=$ROOT_DISK_USED" >> $OUTPUT_FILE
+#echo "ROOT_DISK_AVAIL=$ROOT_DISK_AVAIL" >> $OUTPUT_FILE
+
+echo "<tr>" > $OUTPUT_FILE
+echo "<td width = 80px;>$NAME</td>" >> $OUTPUT_FILE
+echo "<td>$DATE</td>" >> $OUTPUT_FILE
+echo "<td>$NFD_PID</td>" >> $OUTPUT_FILE
+echo "<td>$NFD_VSIZE</td>" >> $OUTPUT_FILE
+echo "<td>$NFD_LOGFILE_SIZE</td>" >> $OUTPUT_FILE
+echo "<td>$ROOT_DISK_SIZE</td>" >> $OUTPUT_FILE
+echo "<td>$ROOT_DISK_USED</td>" >> $OUTPUT_FILE
+echo "<td>$ROOT_DISK_AVAIL</td>" >> $OUTPUT_FILE
+echo "</tr>" >> $OUTPUT_FILE
