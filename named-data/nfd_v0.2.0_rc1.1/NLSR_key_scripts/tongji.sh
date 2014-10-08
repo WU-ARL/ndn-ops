5c784e8bd0a1b972bf8a338d0e7ff99345936bf3
jdehart1 <jdd@wustl.edu>
2014-10-09 03:40:17 +0800

    fix ucla site.cert command for tongji

--- named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/tongji.sh
+++ named-data/nfd_v0.2.0_rc1.1/NLSR_key_scripts/tongji.sh
@@ -3,7 +3,7 @@
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji > ~nlsr/unsigned_site.cert'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI" -s /ndn -p /ndn/cn/edu/tongji -r tongji_unsigned_site.cert > /home/nlsr/site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI" -s /ndn -p /ndn/cn/edu/tongji -r tongji_unsigned_site.cert > /home/nlsr/tongji_site.cert'
 
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI Operator" -s /ndn/cn/edu/tongji -p /ndn/cn/edu/tongji/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
