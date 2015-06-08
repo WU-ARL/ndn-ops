1dba1aa123c787197a1a21fe22f28edbcbd66909
jdehart1 <jdd@wustl.edu>
2015-06-08 14:02:52 -0500

    add logfile

--- NOC/ping_delay_tests/getNeighborDelays.sh
+++ NOC/ping_delay_tests/getNeighborDelays.sh
@@ -6,6 +6,10 @@ PCOUNT=10
 TCOUNT=40
 QUIET=0
 THISNODE="WU"
+
+DATE=`date +%d_%H.%M.%S`
+LOGFILE="getNeighborDelays.$DATE"
+
 while [ $# -ge 1 ]
 do
   if [ $1 = "-p" ]
@@ -40,10 +44,12 @@ done
 
 #echo "LINKS = $LINKS"
 
+echo "Testing Link delays from node $THISNODE" >& $LOGFILE
+
 for l in $LINKS 
 do
 
-  echo " ----------------------------------"
+  echo " ----------------------------------" >> $LOGFILE
   #echo "testing link $l"
 
   PRESULT=`ping -A -q  -c $PCOUNT $l`
@@ -53,12 +59,12 @@ do
   then
     #echo "ping failed, try traceroute"
     #TRESULT=`traceroute  -m $TCOUNT -T -p 6363 $l | tail -1 | sed -e '/ \!H/s///g'`
-    TRESULT=`traceroute  -m $TCOUNT -T -p 6363 $l | tail -1 `
+    TRESULT=`sudo traceroute  -m $TCOUNT -T -p 6363 $l | tail -1 `
     echo "$TRESULT" | grep " \![HSPXVC]"  >& /dev/null
     if [ $? -eq 0 ]
     then
-      echo "ping and traceroute failed, giving up on $THISNODE: NEIGHBOR $l"
-      echo "$TRESULT"
+      echo "ping and traceroute failed, giving up on $THISNODE: NEIGHBOR $l" >> $LOGFILE
+      echo "$TRESULT" >> $LOGFILE
     else
       #echo $TRESULT
       TP1=`echo $TRESULT | cut -d ' ' -f 4`
@@ -90,7 +96,7 @@ do
       fi
   
       #echo "NEIGHBOR $l: AVG: $AVG ($TP1 $TP2 $TP3)"
-      echo "$THISNODE NEIGHBOR $l: MIN: $TMIN AVG: $TAVG MAX: $TMAX "
+      echo "$THISNODE NEIGHBOR $l: MIN: $TMIN AVG: $TAVG MAX: $TMAX " >> $LOGFILE
     fi
   else
     PMIN=`echo $PRESULT | cut -d '/' -f 4-7 | cut -d ' ' -f 3 | cut -d '/' -f 1`
@@ -98,8 +104,8 @@ do
     PMAX=`echo $PRESULT | cut -d '/' -f 4-7 | cut -d ' ' -f 3 | cut -d '/' -f 3`
     PMDEV=`echo $PRESULT | cut -d '/' -f 4-7 | cut -d ' ' -f 3 | cut -d '/' -f 4`
     #echo "NEIGHBOR $l: MIN: $PMIN AVG: $PAVG MAX: $PMAX MDEV: $PMDEV"
-    echo "$THISNODE NEIGHBOR $l: MIN: $PMIN AVG: $PAVG MAX: $PMAX "
+    echo "$THISNODE NEIGHBOR $l: MIN: $PMIN AVG: $PAVG MAX: $PMAX " >> $LOGFILE
   fi
-  echo ""
+  echo "" >> $LOGFILE
 
 done
