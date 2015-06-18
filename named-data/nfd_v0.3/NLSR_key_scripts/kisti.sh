3954f6ff148490c9b9898bd0a1583a190633d8f3
jdehart1 <jdd@wustl.edu>
2015-06-18 13:28:04 -0500

    add config files for KISTI

---
+++ named-data/nfd_v0.3/NLSR_key_scripts/kisti.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# point home to /var/lib/ndn/nlsr so keys will be stored there.
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/kr/kisti > ~nlsr/unsigned_site.cert'
+
+# seems that now we have to set a default identity:
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/kr/kisti/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/kr/kisti/nlsr'
+
+# this next step must take place on UCLA where the root cert is.
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201506180000 -E 201606180000 -N "KISTI" -s /ndn -p /ndn/kr/kisti -r /home/nlsr/kisti_unsigned_site.cert > /home/nlsr/kisti_site.cert'
+
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/kr/kisti/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/kr/kisti/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201506180000 -E 201606180000 -N "KISTI Operator" -s /ndn/kr/kisti -p /ndn/kr/kisti/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/kr/kisti/%C1.Router/router3 > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201506180000 -E 201606180000 -N "KISTI Router router3" -s /ndn/kr/kisti/%C1.Operator/ndnops -p /ndn/kr/kisti/%C1.Router/router3 -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
