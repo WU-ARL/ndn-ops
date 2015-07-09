06af42238315022922875b9cbd601e9a2205e8d6
jdehart1 <jdd@wustl.edu>
2015-07-10 03:24:36 +0800

    update dates for NLSR keys

--- named-data/nfd_v0.3/NLSR_key_scripts/tongji.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/tongji.sh
@@ -2,11 +2,15 @@
 # point home to /var/lib/ndn/nlsr so keys will be stored there.
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji > ~nlsr/unsigned_site.cert'
 
+# seems that now we have to set a default identity:
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/cn/edu/tongji/nlsr'
+
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI" -s /ndn -p /ndn/cn/edu/tongji -r tongji_unsigned_site.cert > /home/nlsr/tongji_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201607080000 -N "TONGJI" -s /ndn -p /ndn/cn/edu/tongji -r tongji_unsigned_site.cert > /home/nlsr/tongji_site.cert'
 
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/cn/edu/tongji/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI Operator" -s /ndn/cn/edu/tongji -p /ndn/cn/edu/tongji/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201607080000 -N "TONGJI Operator" -s /ndn/cn/edu/tongji -p /ndn/cn/edu/tongji/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/cn/edu/tongji/%C1.Router/cnlab > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201407080000 -E 201507080000 -N "TONGJI Router cnlab" -s /ndn/cn/edu/tongji/%C1.Operator/ndnops -p /ndn/cn/edu/tongji/%C1.Router/cnlab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201607080000 -N "TONGJI Router cnlab" -s /ndn/cn/edu/tongji/%C1.Operator/ndnops -p /ndn/cn/edu/tongji/%C1.Router/cnlab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
