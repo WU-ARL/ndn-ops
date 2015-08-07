8f1712dab797c4b713b0c421c7b56e9092d67118
jdehart1 <jdd@wustl.edu>
2015-08-07 12:54:39 -0500

    add new ndn-traffic-server stuff for memphis

---
+++ NOC/memphis-ndn-traffic/check_and_restart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+SERVER_PID=`ps auxwww | grep ndn-traffic-server | grep -v grep | awk '{print $2}'`
+
+#echo "SERVER_PID=$SERVER_PID"
+DATE=`date +%Y.%b.%d.%H.%M.%S`
+LOGFILE=ndn-traffic-server.log.$DATE
+echo "LOGFILE=$LOGFILE"
+HOSTNAME=`hostname`
+
+if [ -z "$SERVER_PID" ]
+then
+  echo "ndn-traffic-server not running, restart it"
+  if [ "$HOSTNAME" = "wundngw" ]
+  then
+    echo "WU"
+    #ndn-traffic-server -q ndn-traffic-server.conf.WU >& $LOGFILE &
+  else
+    echo "Not WU"
+  fi
+else
+  echo "ndn-traffic-server still running, PID=$SERVER_PID"
+fi
+
