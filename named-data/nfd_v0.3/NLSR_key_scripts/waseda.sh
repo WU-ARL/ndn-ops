e04ab0e9831650e612ea0973365e3b8c3a9122b4
jdehart1 <jdd@wustl.edu>
2015-10-08 23:28:13 +0900

    update key script for new keys

--- named-data/nfd_v0.3/NLSR_key_scripts/waseda.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/waseda.sh
@@ -7,10 +7,10 @@ sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/was
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/waseda/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "WU" -s /ndn -p /ndn/jp/waseda -r /home/nlsr/waseda_unsigned_site.cert > /home/nlsr/waseda_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "WU" -s /ndn -p /ndn/jp/waseda -r /home/nlsr/waseda_unsigned_site.cert > /home/nlsr/waseda_site.cert'
 
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/waseda/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/waseda/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "WU Operator" -s /ndn/jp/waseda -p /ndn/jp/waseda/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "WU Operator" -s /ndn/jp/waseda -p /ndn/jp/waseda/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/waseda/%C1.Router/parklab > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "WU Router parklab" -s /ndn/jp/waseda/%C1.Operator/ndnops -p /ndn/jp/waseda/%C1.Router/parklab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "WU Router parklab" -s /ndn/jp/waseda/%C1.Operator/ndnops -p /ndn/jp/waseda/%C1.Router/parklab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
