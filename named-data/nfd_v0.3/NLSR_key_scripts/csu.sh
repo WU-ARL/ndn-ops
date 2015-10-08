e3a2b39692568dd8c9740b29c852bd4e1844d3e6
WU-ARL <jdd@arl.wustl.edu>
2015-10-08 08:34:37 -0600

    update key script for new keys

--- named-data/nfd_v0.3/NLSR_key_scripts/csu.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/csu.sh
@@ -7,10 +7,10 @@ sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/co
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/edu/colostate/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "CSU" -s /ndn -p /ndn/edu/colostate -r /home/nlsr/csu_unsigned_site.cert > /home/nlsr/csu_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "CSU" -s /ndn -p /ndn/edu/colostate -r /home/nlsr/csu_unsigned_site.cert > /home/nlsr/csu_site.cert'
 
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/colostate/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/edu/colostate/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "CSU Operator" -s /ndn/edu/colostate -p /ndn/edu/colostate/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "CSU Operator" -s /ndn/edu/colostate -p /ndn/edu/colostate/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/colostate/%C1.Router/mccoy > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "CSU Router mccoy" -s /ndn/edu/colostate/%C1.Operator/ndnops -p /ndn/edu/colostate/%C1.Router/mccoy -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "CSU Router mccoy" -s /ndn/edu/colostate/%C1.Operator/ndnops -p /ndn/edu/colostate/%C1.Router/mccoy -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
