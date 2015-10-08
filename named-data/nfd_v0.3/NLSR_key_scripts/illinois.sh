4afc64aec6e398c0727f2c1edb80189173aa3650
jdehart1 <jdd@wustl.edu>
2015-10-08 09:35:06 -0500

    update key script for new keys

--- named-data/nfd_v0.3/NLSR_key_scripts/illinois.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/illinois.sh
@@ -7,10 +7,10 @@ sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/il
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/edu/illinois/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "UIUC" -s /ndn -p /ndn/edu/illinois -r /home/nlsr/illinois_unsigned_site.cert > /home/nlsr/illinois_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "UIUC" -s /ndn -p /ndn/edu/illinois -r /home/nlsr/illinois_unsigned_site.cert > /home/nlsr/illinois_site.cert'
 
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/illinois/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/edu/illinois/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "UIUC Operator" -s /ndn/edu/illinois -p /ndn/edu/illinois/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "UIUC Operator" -s /ndn/edu/illinois -p /ndn/edu/illinois/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
 sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/edu/illinois/%C1.Router/ndnx > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201610080000 -N "UIUC Router ndnx" -s /ndn/edu/illinois/%C1.Operator/ndnops -p /ndn/edu/illinois/%C1.Router/ndnx -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "UIUC Router ndnx" -s /ndn/edu/illinois/%C1.Operator/ndnops -p /ndn/edu/illinois/%C1.Router/ndnx -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
