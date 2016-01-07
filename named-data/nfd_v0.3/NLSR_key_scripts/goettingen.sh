99a2165005399a58423bcb8db14b9fe5ca70e763
jdehart1 <jdd@wustl.edu>
2016-01-07 17:56:45 +0100

    nlsr key script file for goettingen

---
+++ named-data/nfd_v0.3/NLSR_key_scripts/goettingen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# point home to /var/lib/ndn/nlsr so keys will be stored there.
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/de/goettingen > ~nlsr/unsigned_site.cert'
+
+# seems that now we have to set a default identity:
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/de/goettingen/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/de/goettingen/nlsr'
+
+# this next step must take place on UCLA where the root cert is.
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "GOETTINGEN" -s /ndn -p /ndn/de/goettingen -r /home/nlsr/goettingen_unsigned_site.cert > /home/nlsr/goettingen_site.cert'
+
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/de/goettingen/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/de/goettingen/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "GOETTINGEN Operator" -s /ndn/de/goettingen -p /ndn/de/goettingen/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/de/goettingen/%C1.Router/ndntestbed > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "GOETTINGEN Router ndntestbed" -s /ndn/de/goettingen/%C1.Operator/ndnops -p /ndn/de/goettingen/%C1.Router/ndntestbed -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
