00bcf2e0a47726d1e710ac9af83fe360ccce3219
jdehart1 <jdd@wustl.edu>
2016-03-18 04:03:38 +0700

    update indonesia with new prefix

--- named-data/nfd_v0.3/NLSR_key_scripts/indonesia.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/indonesia.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # point home to /var/lib/ndn/nlsr so keys will be stored there.
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/uindonesia > ~nlsr/unsigned_site.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/ac/ui > ~nlsr/unsigned_site.cert'
 
 # seems that now we have to set a default identity:
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/uindonesia/nlsr > ~nlsr/unsigned_nlsr.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/id/uindonesia/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/ac/ui/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/id/ac/ui/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "UINDONESIA" -s /ndn -p /ndn/id/uindonesia -r /home/nlsr/indonesia_unsigned_site.cert > /home/nlsr/indonesia_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "UINDONESIA" -s /ndn -p /ndn/id/ac/ui -r /home/nlsr/indonesia_unsigned_site.cert > /home/nlsr/indonesia_site.cert'
 
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/uindonesia/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/id/uindonesia/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "INDONESIA Operator" -s /ndn/id/uindonesia -p /ndn/id/uindonesia/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/uindonesia/%C1.Router/ndnlab-s5-1420d > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "INDONESIA Router ndnlab-s5-1420d" -s /ndn/id/uindonesia/%C1.Operator/ndnops -p /ndn/id/uindonesia/%C1.Router/ndnlab-s5-1420d -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/ac/ui/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/id/ac/ui/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "INDONESIA Operator" -s /ndn/id/ac/ui -p /ndn/id/ac/ui/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/id/ac/ui/%C1.Router/ndnlab-s5-1420d > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "INDONESIA Router ndnlab-s5-1420d" -s /ndn/id/ac/ui/%C1.Operator/ndnops -p /ndn/id/ac/ui/%C1.Router/ndnlab-s5-1420d -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
