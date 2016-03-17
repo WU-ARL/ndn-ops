97617945ecab0f671027e47f5c74ff131e1390c9
jdehart1 <jdd@wustl.edu>
2016-03-17 12:44:21 -0500

    update prefixes for goettingen minho and osaka

--- named-data/nfd_v0.3/NLSR_key_scripts/minho.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/minho.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # point home to /var/lib/ndn/nlsr so keys will be stored there.
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/minho > ~nlsr/unsigned_site.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/uminho > ~nlsr/unsigned_site.cert'
 
 # seems that now we have to set a default identity:
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/minho/nlsr > ~nlsr/unsigned_nlsr.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/pt/minho/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/uminho/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/pt/uminho/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO" -s /ndn -p /ndn/pt/minho -r /home/nlsr/minho_unsigned_site.cert > /home/nlsr/minho_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO" -s /ndn -p /ndn/pt/uminho -r /home/nlsr/minho_unsigned_site.cert > /home/nlsr/minho_site.cert'
 
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/minho/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/pt/minho/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO Operator" -s /ndn/pt/minho -p /ndn/pt/minho/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/minho/%C1.Router/vnetlab > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO Router vnetlab" -s /ndn/pt/minho/%C1.Operator/ndnops -p /ndn/pt/minho/%C1.Router/vnetlab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/uminho/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/pt/uminho/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO Operator" -s /ndn/pt/uminho -p /ndn/pt/uminho/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/pt/uminho/%C1.Router/vnetlab > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "MINHO Router vnetlab" -s /ndn/pt/uminho/%C1.Operator/ndnops -p /ndn/pt/uminho/%C1.Router/vnetlab -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
--- named-data/nfd_v0.3/NLSR_key_scripts/osaka.sh
+++ named-data/nfd_v0.3/NLSR_key_scripts/osaka.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # point home to /var/lib/ndn/nlsr so keys will be stored there.
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/osaka > ~nlsr/unsigned_site.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/ac/osaka-u > ~nlsr/unsigned_site.cert'
 
 # seems that now we have to set a default identity:
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/osaka/nlsr > ~nlsr/unsigned_nlsr.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/osaka/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/ac/osaka-u/nlsr > ~nlsr/unsigned_nlsr.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/ac/osaka-u/nlsr'
 
 # this next step must take place on UCLA where the root cert is.
-# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA" -s /ndn -p /ndn/jp/osaka -r /home/nlsr/osaka_unsigned_site.cert > /home/nlsr/osaka_site.cert'
+# sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA" -s /ndn -p /ndn/jp/ac/osaka-u -r /home/nlsr/osaka_unsigned_site.cert > /home/nlsr/osaka_site.cert'
 
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/osaka/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/osaka/nlsr'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA Operator" -s /ndn/jp/osaka -p /ndn/jp/osaka/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/osaka/%C1.Router/NDNtestbed > ~nlsr/unsigned_router.cert'
-sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA Router NDNtestbed" -s /ndn/jp/osaka/%C1.Operator/ndnops -p /ndn/jp/osaka/%C1.Router/NDNtestbed -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/ac/osaka-u/%C1.Operator/ndnops > ~nlsr/unsigned_operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-set-default -n /ndn/jp/ac/osaka-u/nlsr'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA Operator" -s /ndn/jp/ac/osaka-u -p /ndn/jp/ac/osaka-u/%C1.Operator/ndnops -r ~nlsr/unsigned_operator.cert > /etc/ndn/nlsr/keys/operator.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-key-gen -n /ndn/jp/ac/osaka-u/%C1.Router/NDNtestbed > ~nlsr/unsigned_router.cert'
+sudo su - nlsr -c 'export HOME=/var/lib/ndn/nlsr/; ndnsec-cert-gen -S 201507080000 -E 201810080000 -N "OSAKA Router NDNtestbed" -s /ndn/jp/ac/osaka-u/%C1.Operator/ndnops -p /ndn/jp/ac/osaka-u/%C1.Router/NDNtestbed -r ~nlsr/unsigned_router.cert > /etc/ndn/nlsr/keys/router.cert'
