963fd8a91518acc734541172237eebf39ebe6ac4
jdehart1 <jdd@wustl.edu>
2016-08-17 10:48:26 -0500

    force install of add-apt-repo tools

--- NOC/ubuntu_installs.sh
+++ NOC/ubuntu_installs.sh
@@ -6,6 +6,7 @@ sudo apt-get -y install build-essential ant git openjdk-6-jdk openjdk-6-jre open
 sudo apt-get -y install gawk texinfo asciidoc finger
 
 sudo apt-get -y install python-software-properties
+sudo apt-get -y install software-properties-common 
 
 sudo apt-get -y install ntp
 
